83c2176adc815ee458a801fe1946c9798ad921e5
kimsch <rauko1@gmail.com>
2017-06-01 00:29:27 +0200

    small changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -15,10 +15,10 @@
             <li><a href="/shops/create">Create new</a></li>
           </ul>
         </li>
-@else
-		<li><a href="/shops/edit/{{ Auth::user()->shop_id }}"><i class="fa fa-dashboard"></i> <span>Shops</span></a></li>
+@elseif(Auth::user()->access_level_id == 2)
+		<li><a href="/shops/edit/{{ Auth::user()->shop_id }}"><i class="fa fa-dashboard"></i> <span>Shop Info</span></a></li>
 @endif
-@if((Auth::user()->shop_id != null || Session::get('shopId') != null) && Auth::user()->access_level_id != 3)
+@if((Auth::user()->shop_id != null || Session::get('shopId') != null)
         <li class="treeview">
           <a href="#"><i class="fa fa-shopping-cart"></i> <span>Products</span>
             <span class="pull-right-container">
